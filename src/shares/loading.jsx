2920c6a51dd625116330c92f54b8e22abd47c2f1
Ziehuog <bdieuhuong10@gmail.com>
2022-09-06 09:42:37 +0700

    6/9

--- src/shares/loading.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
-
-function Loading({ type, color }) {
-  return <ReactLoading type={type} color={color} height={667} width={375} />;
-}
-
-export default Loading;
